17b1c588c1554cbcb98ce249aada022afd36efe2
ikumadaiki <ikuma.daiki.infratop@gmail.com>
2021-07-22 02:02:48 +0000

    [Finish] 3-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,15 +4,18 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
+    @blog = Blog.new
+    
   end
   
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path(blog.id)
     
   end
 
